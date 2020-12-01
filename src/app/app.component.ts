0063e272cf0364fca61d728bb1470f52ca7e7943
Angela Mitchell <anglarenmit@gmail.com>
2020-11-30 22:47:26 -0600

    Implemented checkUsername function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username: string = '';
+  disableButton: boolean = true;
 
   constructor() {
+  }
+
+  checkUsername() {
+    if (this.username === '') {
+      this.disableButton = true;
+    } else {
+      this.disableButton = false;
+    }
   }
 
   resetUsername() {
     if (this.username !== '') {
       this.username = '';
+      this.disableButton = true;
     }
   }
 }
